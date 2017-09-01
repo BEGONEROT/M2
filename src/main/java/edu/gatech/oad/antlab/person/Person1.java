346c780e37b165551161847aaf4325739d5b0cf3
Aadarsh Padiyath <aspadiyath@gmail.com>
2017-08-31 22:50:50 -0400

    Completed Person1.java

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,13 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		if (input.length() == 0) {
+			return "";
+		} else if (input.length() == 1 || input.length() == 2) {
+			return input;
+		} else {
+			return substring(2, input.length()) + substring(0, 2);
+		}
 	}
 	
 	/**
